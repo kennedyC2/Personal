6f1b4a906a27cda8df7666258f1b5b4ff88d3ee4
kennedyC2 <kennedychidi55@gmail.com>
2023-12-19 21:50:35 +0100

    updated

--- app.py
+++ app.py
@@ -8,18 +8,6 @@ import ssl
 # Instantiate
 app = Flask(__name__)
 
-# Cert
-context = ssl.SSLContext(ssl.PROTOCOL_TLS)
-context.load_cert_chain("./https/cert.pem", "./https/key.pem")
-
-
-# Enforce HTTPS
-@app.before_request
-def checkProtocol():
-    if not request.is_secure:
-        url = request.url.replace("http://", "https://")
-        return redirect(url, code=302)
-
 
 # Home
 @app.route("/", methods=["GET"])
@@ -37,5 +25,5 @@ def fav():
     return
 
 
-if __name__ == "_main_":
-    app.run(host="0.0.0.0", port=4550, debug=True, ssl_context=context)
+if __name__ == '__main__':
+    app.run(host="127.0.0.1", port=5000, debug=True, use_reloader=True)
--- static/js/app.js
+++ static/js/app.js
@@ -2,6 +2,7 @@ const navItems_1 = document.querySelectorAll("li.bt");
 const navItems_2 = document.querySelectorAll("li.mt");
 const nav_m = document.getElementById("nav_m");
 const pgs = [document.getElementById("hm"), document.getElementById("abt"), document.getElementById("wk"), document.getElementById("pgt"), document.getElementById("cont")];
+const logo = document.getElementById("tp")
 
 // Swap Pages
 const Swap_d = () => {
@@ -52,35 +53,46 @@ const Swap_m = () => {
 
             switch (nav.classList[1]) {
                 case "h":
+                    logo.classList.remove("closed")
                     pgs[0].classList.remove("closed");
+                    nav_m.classList.remove("out");
+                    pgs[0].style.height = "85%"
                     setTimeout(() => {
                         nav_m.classList.add("out");
                     }, 1000);
                     break;
 
                 case "i":
+                    logo.classList.add("closed")
                     pgs[1].classList.remove("closed");
+                    pgs[1].style.height = "auto"
                     setTimeout(() => {
                         nav_m.classList.add("out");
                     }, 1000);
                     break;
 
                 case "w":
+                    logo.classList.add("closed")
                     pgs[2].classList.remove("closed");
+                    pgs[2].style.height = "auto"
                     setTimeout(() => {
                         nav_m.classList.add("out");
                     }, 1000);
                     break;
 
                 case "p":
+                    logo.classList.add("closed")
                     pgs[3].classList.remove("closed");
+                    pgs[3].style.height = "auto"
                     setTimeout(() => {
                         nav_m.classList.add("out");
                     }, 1000);
                     break;
 
                 case "c":
+                    logo.classList.add("closed")
                     pgs[4].classList.remove("closed");
+                    pgs[4].style.height = "auto"
                     setTimeout(() => {
                         nav_m.classList.add("out");
                     }, 1000);
@@ -95,30 +107,25 @@ const Swap_m = () => {
 
 // Space
 const space = () => {
+    let position = 0
     window.addEventListener("touchstart", () => {
         if (nav_m.classList.contains("out")) {
             nav_m.classList.remove("out");
-            setTimeout(() => {
-                nav_m.classList.add("out");
-            }, 1000 * 5);
         }
     });
 
     window.addEventListener("scroll", () => {
-        console.log(window.scrollY);
-        if (window.scrollY > 0) {
+        console.log("hello")
+        if (window.scrollY - position > 0) {
             if (!nav_m.classList.contains("out")) {
                 nav_m.classList.add("out");
             }
-        }
-
-        if (window.scrollY == 0) {
+            position = window.scrollY
+        } else {
             if (nav_m.classList.contains("out")) {
                 nav_m.classList.remove("out");
-                setTimeout(() => {
-                    nav_m.classList.add("out");
-                }, 1000 * 5);
             }
+            position = window.scrollY
         }
     });
 };
